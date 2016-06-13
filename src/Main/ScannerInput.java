a96c52b6b789d0008efb57c67893a21e969e00a0
navreet <navreet@improvonia.com>
2016-06-13 16:47:20 -0400

    updated readme

--- src/Main/ScannerInput.java
+++ src/Main/ScannerInput.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class ScannerInput {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.println("How is the weather?");
+        String answer = sc.nextLine();
+
+        if (answer.equals("rain"))
+            System.out.println("Take your umbrella!");
+        else if (answer.equals("windy") )
+            System.out.println("Wear your jacket!"); 
+        else if (answer.equals("snow")) 
+            System.out.println("Wear a coat and take a shovel!"); 
+
+        else if (answer.equals("stormy")) 
+            System.out.println("You should stay inside!"); 
+        else 
+            System.out.println("Enjoy your day!");
+        sc.close();
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Enter your test score: ");
 		int score = keyboard.nextInt();
