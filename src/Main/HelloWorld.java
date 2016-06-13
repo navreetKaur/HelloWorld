f033b08781c88f21faefc22a8af87acbecd48ffc
navreet <navreet@improvonia.com>
2016-06-13 11:03:14 -0400

    Created Hello World Java Files

---
+++ src/Main/HelloWorld.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("Today is June 13th, 2016. I am alive!");
+		System.out.println("My name is Navreet");
+	}
+
+}
---
+++ src/Main/NameAgePrint.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class NameAgePrint {
+
+	public static void main(String[] args) {
+		//Name:Navreet and Age: 26
+		System.out.println("Navreet");
+
+	}
+
+}
