235c18589a6643fd6cf4b7232378390a79b2398b
navreet <navreet@improvonia.com>
2016-06-14 14:39:07 -0400

    updated adventure

---
+++ src/DiceRoll/DiceRoller.java
@@ -0,0 +1,16 @@
+package DiceRoll;
+
+import java.util.Random;
+
+public class DiceRoller
+ {
+    public static void main( String[] args )
+    {
+        int dienumber;
+        Random rnd = new Random();
+        
+        dienumber =  1 + rnd.nextInt(6);
+        
+        System.out.println("Your die roll was : " + dienumber);
+    }
+ }
